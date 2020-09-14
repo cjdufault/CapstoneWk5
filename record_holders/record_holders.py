649c03c9bf1dce6ba393d848be7a1f5b1b619d10
cjdufault <dufaultconnor@gmail.com>
2020-09-14 12:00:21 -0500

    implement add, update, delete, and search functions

--- record_holders/record_holders.py
+++ record_holders/record_holders.py
@@ -5,14 +5,48 @@ db = SqliteDatabase('records_db.sqlite')
 
 
 def main():
+    db.connect()
+    db.create_tables([Record])
+    populate_db()
     
 
+def add_record(name_str, country_str, catches_int):
+    new_record = Record(name=name_str, country=country_str, catches=catches_int)
+    new_record.save()
+    
 
-class Record:
-    def __init__(self, name, country, catches):
-        self.name = name
-        self.country = country
-        self.catches = catches
+def update_record(name_str, catches_int):
+    rows_changed = Record.update(catches=catches_int).where(Record.name == name_str).execute()
+    return rows_changed
+    
+    
+def delete_record(name_str):
+    rows_deleted = Record.delete().where(Record.name == name_str).execute()
+    return rows_deleted
+    
+
+# returns all records where 'name' matches search_name
+def search(name_str):
+    rows_selected = Record.select().where(Record.name == name_str)
+    return rows_selected
+    
+
+# fills db with example data
+def populate_db():
+    add_record('Janne Mustonen', 'Finland', 98)
+    add_record('Ian Stewart', 'Canada', 94)
+    add_record('Aaron Gregg', 'Canada', 88)
+    add_record('Chad Taylor', 'USA', 78)
+
+
+# model class for records to be stored in database
+class Record(Model):
+    name = CharField()
+    country = CharField()
+    catches = IntegerField()
+    
+    class Meta:
+        database = db
     
     def __str__(self):
         return f'Name: {self.name} Country: {self.country} Number of Catches: {self.catches}'
